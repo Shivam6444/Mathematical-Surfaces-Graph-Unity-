d7fc3813eb943b0bfeec147ee103b35546d55464
Shivam6444 <33787741+Shivam6444@users.noreply.github.com>
2018-05-16 17:38:41 -0300

    Domain Change

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -7,11 +7,16 @@ public class Graph : MonoBehaviour {
     public Transform pointPrefab;
 
     public void Awake(){
-        
-        for(int i = 0; i < 10; i++ ){
+        Vector3 scale = Vector3.one / 5f;
+        Vector3 position;
+        position.y = 0f;
+        position.z = 0f;
+        for (int i = 0; i < 10; i++ ){
             Transform point = Instantiate(pointPrefab);
-            point.localPosition = Vector3.right *i;
-           
+            //  point.localPosition = Vector3.right *((i + 0.5f) /5f - 1f);
+            position.x = i + 0.5f / 5f - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
         }
 
 
