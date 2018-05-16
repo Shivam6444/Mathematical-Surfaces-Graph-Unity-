b8d5dd1194221be87cb1178b87b18b163c731fc9
Shivam6444 <33787741+Shivam6444@users.noreply.github.com>
2018-05-16 20:11:52 -0300

    Updating Points

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -5,24 +5,32 @@ using UnityEngine;
 public class Graph : MonoBehaviour {
 
     public Transform pointPrefab;
+    Transform[] points;
     [Range(10,100)]public int resolution = 10;
-
+    //Range- the slider in inspector.
     public void Awake(){
-        float step = 2f / resolution;
+        points = new Transform[resolution];//Just like instansiation of points object.
+        float step = 2f / resolution; //Calculation of step according to current resolution.
         Vector3 scale = Vector3.one * step;
         Vector3 position;
-        
         position.z = 0f;
-        for (int i = 0; i < resolution; i++ ){
+        position.y = 0f;
+        for (int i = 0; i < resolution; i++){
             Transform point = Instantiate(pointPrefab);
-            //  point.localPosition = Vector3.right *((i + 0.5f) /5f - 1f);
             position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x * position.x;//THIS IS FUNCTION
             point.localPosition = position;
             point.localScale = scale;
             point.SetParent(transform, false);
+            points[i] = point;
         }
+    }
 
-
+    void Update(){
+        for(int i = 0; i < points.Length; i++){
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            position.y = Mathf.Sin(position.x);//This is our function.
+            point.localPosition = position;
+        }
     }
 }
