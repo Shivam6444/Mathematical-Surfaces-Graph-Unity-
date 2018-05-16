469843228e442f77fc4a4fb809877c303e756cad
Shivam6444 <33787741+Shivam6444@users.noreply.github.com>
2018-05-16 18:07:18 -0300

    Resolution

--- Assets/Graph.cs
+++ Assets/Graph.cs
@@ -5,16 +5,20 @@ using UnityEngine;
 public class Graph : MonoBehaviour {
 
     public Transform pointPrefab;
+    [Range(10,100)]public int resolution = 10;
 
     public void Awake(){
-        Vector3 scale = Vector3.one / 5f;
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
         Vector3 position;
-        position.y = 0f;
+        
         position.z = 0f;
-        for (int i = 0; i < 10; i++ ){
+        for (int i = 0; i < resolution; i++ ){
+
             Transform point = Instantiate(pointPrefab);
             //  point.localPosition = Vector3.right *((i + 0.5f) /5f - 1f);
-            position.x = i + 0.5f / 5f - 1f;
+            position.x = (i + 0.5f) * step - 1f;
+            position.y = position.x * position.x;//THIS IS FUNCTION
             point.localPosition = position;
             point.localScale = scale;
         }
