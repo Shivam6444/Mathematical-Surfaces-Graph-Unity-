8c79524f7b3f1ed495bb8af09a39f2825af94c11
Shivam6444 <33787741+Shivam6444@users.noreply.github.com>
2018-05-16 15:18:12 -0300

    Prefab

---
+++ Assets/Graph.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Graph : MonoBehaviour {
+
+    public Transform pointPrefab;
+
+    public void Awake(){
+        Instantiate(pointPrefab);
+    }
+}
